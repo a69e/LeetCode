9e8cdd8e23fc2ad8a5114742c8f52c9a850cc282
a69e <lawoutla@gmail.com>
2021-12-24 19:49:16 -0500

    20211224

---
+++ Tree/297.serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,127 @@
+#
+# @lc app=leetcode id=297 lang=python3
+#
+# [297] Serialize and Deserialize Binary Tree
+#
+# https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+#
+# algorithms
+# Hard (52.49%)
+# Likes:    5680
+# Dislikes: 228
+# Total Accepted:    536.8K
+# Total Submissions: 1M
+# Testcase Example:  '[1,2,3,null,null,4,5]'
+#
+# Serialization is the process of converting a data structure or object into a
+# sequence of bits so that it can be stored in a file or memory buffer, or
+# transmitted across a network connection link to be reconstructed later in the
+# same or another computer environment.
+# 
+# Design an algorithm to serialize and deserialize a binary tree. There is no
+# restriction on how your serialization/deserialization algorithm should work.
+# You just need to ensure that a binary tree can be serialized to a string and
+# this string can be deserialized to the original tree structure.
+# 
+# Clarification: The input/output format is the same as how LeetCode serializes
+# a binary tree. You do not necessarily need to follow this format, so please
+# be creative and come up with different approaches yourself.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [1,2,3,null,null,4,5]
+# Output: [1,2,3,null,null,4,5]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = []
+# Output: []
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the tree is in the range [0, 10^4].
+# -1000 <= Node.val <= 1000
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        data = []
+
+        def recurse(node):
+            if not node:
+                data.append('#')
+                return
+            data.append(str(node.val))
+            recurse(node.left)
+            recurse(node.right)
+        
+        recurse(root)
+        return ','.join(data)
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = list(reversed(data.split(',')))
+
+        def recurse(data: list):
+            if data:
+                num = data.pop()
+                if num == '#':
+                    return None
+                node = TreeNode(num)
+                node.left = recurse(data)
+                node.right = recurse(data)
+                return node
+
+        return recurse(data)
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+# @lc code=end
+
+
+# Test
+testcase = [1,2,3,None,None,4,5]
+
+def genTreeNode(treenode, lst, index):
+    if index < len(lst):
+        if lst[index] is None:
+            return None
+        else:
+            treenode = TreeNode(lst[index])
+            treenode.left = genTreeNode(treenode.left, lst, 2 * index + 1)
+            treenode.right = genTreeNode(treenode.right, lst, 2 * index + 2)
+            return treenode
+    return treenode
+root = genTreeNode(TreeNode(0), testcase, 0)
+
+print(Codec().deserialize(Codec().serialize(root)).left.right.val)
---
+++ Tree/652.find-duplicate-subtrees.py
@@ -0,0 +1,104 @@
+#
+# @lc app=leetcode id=652 lang=python3
+#
+# [652] Find Duplicate Subtrees
+#
+# https://leetcode.com/problems/find-duplicate-subtrees/description/
+#
+# algorithms
+# Medium (55.00%)
+# Likes:    2746
+# Dislikes: 275
+# Total Accepted:    126.6K
+# Total Submissions: 230K
+# Testcase Example:  '[1,2,3,4,null,2,4,null,null,4]'
+#
+# Given the root of a binary tree, return all duplicate subtrees.
+# 
+# For each kind of duplicate subtrees, you only need to return the root node of
+# any one of them.
+# 
+# Two trees are duplicate if they have the same structure with the same node
+# values.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [1,2,3,4,null,2,4,null,null,4]
+# Output: [[2,4],[4]]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [2,1,1]
+# Output: [[1]]
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: root = [2,2,2,3,null,3,null]
+# Output: [[2,3],[3]]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of the nodes in the tree will be in the range [1, 10^4]
+# -200 <= Node.val <= 200
+# 
+# 
+#
+
+
+from typing import List, Optional
+
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        ans, hash = [], {}
+        
+        def recurse(node: Optional[TreeNode]):
+            if not node:
+                return '*'
+            left = recurse(node.left)
+            right = recurse(node.right)
+            subtree = left + '-' + right + '-' + str(node.val)
+            hash[subtree] = hash.get(subtree, 0) + 1
+            if hash[subtree] == 2:
+                ans.append(node)
+            return subtree
+        
+        recurse(root)
+        return ans
+
+# @lc code=end
+
+
+# Test
+testcase = [0,0,0,0,None,None,0,None,None,None,0]
+
+def genTreeNode(treenode, lst, index):
+    if index < len(lst):
+        if lst[index] is None:
+            return None
+        else:
+            treenode = TreeNode(lst[index])
+            treenode.left = genTreeNode(treenode.left, lst, 2 * index + 1)
+            treenode.right = genTreeNode(treenode.right, lst, 2 * index + 2)
+            return treenode
+    return treenode
+root = genTreeNode(TreeNode(), testcase, 0)
+
+print(Solution().findDuplicateSubtrees(root))
